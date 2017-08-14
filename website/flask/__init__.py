c90b5d67fae8fad5c85ff974d567e88b24a3650e
denis-torre <denis.torre@mssm.edu>
2017-08-14 16:04:45 -0400

    Updating

--- website/flask/__init__.py
+++ website/flask/__init__.py
@@ -16,6 +16,11 @@ db = SQLAlchemy(app)
 engine = db.engine
 entry_point = '/datasets2tools'
 
+# Lists
+tool_list = [{'tool_name': 'Enrichr','date':'May 27th, 2017','tool_icon_url': 'http://amp.pharm.mssm.edu/Enrichr/images/enrichr-icon.png','tool_description': 'An enrichment analysis tool with over 90 libraries.','has_scripts': True},{'tool_name': 'L1000CDS2','date':'May 27th, 2017','tool_icon_url': 'http://amp.pharm.mssm.edu/Enrichr/images/enrichr-icon.png','tool_description': 'An enrichment analysis tool with over 90 libraries.','has_scripts': True},{'tool_name': 'GeneMANIA','date':'May 27th, 2017','tool_icon_url': 'http://amp.pharm.mssm.edu/Enrichr/images/enrichr-icon.png','tool_description': 'An enrichment analysis tool with over 90 libraries.','has_scripts': True}]
+canned_analysis_list = [{'canned_analysis_accession':'DCA0000178','canned_analysis_preview_url': 'https://github.com/denis-torre/images/blob/master/genemania/1.png?raw=true','canned_analysis_title': 'Enrichment analysis of genes upregulated in cancer','tools': [{'tool_name': 'GeneMANIA','tool_icon_url': 'https://pbs.twimg.com/profile_images/720745173970460672/psYcEwIT_400x400.jpg'},{'tool_name': 'Enrichr','tool_icon_url': 'http://amp.pharm.mssm.edu/Enrichr/images/enrichr-icon.png'},{'tool_name': 'L1000CDS2','tool_icon_url': 'http://amp.pharm.mssm.edu/L1000CDS2/CSS/images/sigine.png'}],'datasets': [{'dataset_accession': 'GSE68203','repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif'},{'dataset_accession': 'GSE68205','repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif'},{'dataset_accession': 'GSE68209','repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif'}],'metadata': {'alpha': 'beta','gamma': 'delta','epsilon': 'zeta'}}]
+dataset_list = [{'dataset_accession': 'GSE68203','dataset_type': 'RNA-seq','dataset_title': 'Genome-wide RNA Pol II-CTD occupancy in wild type and mutant worms','dataset_description': 'We used RNA Pol II-CTD occupancy assay to identify mRNA isoform switch in mutant animals compared to wild type animals.','repository_name': 'Gene Expression Omnibus','repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif','repository_homepage_url': 'https://www.ncbi.nlm.nih.gov/geo/','date': 'May 27th, 2017'}]
+
 # Login
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -75,65 +80,16 @@ def index():
 
 @app.route(entry_point+'/dataset')
 def dataset_landing():
-	dataset = {
-		'dataset_accession': 'GSE68203',
-		'dataset_type': 'RNA-seq',
-		'dataset_title': 'Genome-wide RNA Pol II-CTD occupancy in wild type and mutant worms',
-		'dataset_description': 'We used RNA Pol II-CTD occupancy assay to identify mRNA isoform switch in mutant animals compared to wild type animals.',
-		'repository_name': 'Gene Expression Omnibus',
-		'repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif',
-		'repository_homepage_url': 'https://www.ncbi.nlm.nih.gov/geo/',
-		'date': 'May 27th, 2017'
-	}
-	canned_analysis_list = [
-		{
-			'canned_analysis_preview_url': 'https://github.com/denis-torre/images/blob/master/genemania/1.png?raw=true',
-			'canned_analysis_title': 'Enrichment analysis of genes upregulated in cancer',
-			'tools': [
-				{
-					'tool_name': 'GeneMANIA',
-					'tool_icon_url': 'https://pbs.twimg.com/profile_images/720745173970460672/psYcEwIT_400x400.jpg'
-				},
-				{
-					'tool_name': 'Enrichr',
-					'tool_icon_url': 'http://amp.pharm.mssm.edu/Enrichr/images/enrichr-icon.png'
-				},
-				{
-					'tool_name': 'L1000CDS2',
-					'tool_icon_url': 'http://amp.pharm.mssm.edu/L1000CDS2/CSS/images/sigine.png'
-				}],
-			'datasets': [
-				{
-					'dataset_accession': 'GSE68203',
-					'repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif'
-				},
-				{
-					'dataset_accession': 'GSE68205',
-					'repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif'
-				},
-				{
-					'dataset_accession': 'GSE68209',
-					'repository_icon_url': 'https://www.ncbi.nlm.nih.gov/geo/img/geo_main.gif'
-				}
-			],
-			'metadata': {
-				'alpha': 'beta',
-				'gamma': 'delta',
-				'epsilon': 'zeta'
-			}
-		}
-	]
+	object_data = {'dataset': dataset_list[0], 'canned_analyses': canned_analysis_list, 'tools': tool_list}
+	return render_template('dataset_landing.html', object_data=object_data)
 
-	tool_list = [
-		{
-			'tool_name': 'Enrichr',
-			'tool_icon_url': 'http://amp.pharm.mssm.edu/Enrichr/images/enrichr-icon.png',
-			'tool_description': 'An enrichment analysis tool with over 90 libraries.',
-			'has_scripts': True
-		}
-	]
-	fairness_dataframe = pd.DataFrame([[1, 'The tool is hosted in one or more well-used repositories, if relevant repositories exist.', "{:0.1f}%".format(random.uniform(0,1)*100)],[2, 'Source code is shared on a public repository.', "{:0.1f}%".format(random.uniform(0,1)*100)],[3, 'Code is written in an open-source, free programming language.', "{:0.1f}%".format(random.uniform(0,1)*100)],[4, 'The tool inputs standard data format(s) consistent with community practice.', "{:0.1f}%".format(random.uniform(0,1)*100)],[5, 'All previous versions of the tool are made available.', "{:0.1f}%".format(random.uniform(0,1)*100)],[6, 'Web-based version is available (in addition to desktop version).', "{:0.1f}%".format(random.uniform(0,1)*100)],[7, 'Source code is documented.', "{:0.1f}%".format(random.uniform(0,1)*100)],[8, 'Pipelines that use the tool have been standardized and provide detailed usage guidelines.', "{:0.1f}%".format(random.uniform(0,1)*100)],[9, 'A tutorial page is provided for the tool.', "{:0.1f}%".format(random.uniform(0,1)*100)],[10, 'Example datasets are provided.', "{:0.1f}%".format(random.uniform(0,1)*100)],[11, 'Licensing information is provided on the tool\'s landing page.', "{:0.1f}%".format(random.uniform(0,1)*100)],[12, 'Information is provided describing how to cite the tool.', "{:0.1f}%".format(random.uniform(0,1)*100)],[13, 'Version information is provided for the tool.', "{:0.1f}%".format(random.uniform(0,1)*100)],[14, 'A paper about the tool has been published.', "{:0.1f}%".format(random.uniform(0,1)*100)],[15, 'Video tutorials for the tool are available.', "{:0.1f}%".format(random.uniform(0,1)*100)],[16, 'Contact information is provided for the originator(s) of the tool.', "{:0.1f}%".format(random.uniform(0,1)*100)]],columns=['#', 'FAIRness Question', 'Score'])
-	return render_template('dataset_landing.html', dataset=dataset, canned_analysis_list=canned_analysis_list, tool_list=tool_list, fairness_dataframe=fairness_dataframe)
+@app.route(entry_point+'/tool')
+def tool_landing():
+	return render_template('tool_landing.html', tool=tool_list[0], canned_analysis_list=canned_analysis_list, dataset_list=dataset_list)
+
+@app.route(entry_point+'/analysis')
+def analysis_landing():
+	return render_template('analysis_landing.html', canned_analysis=canned_analysis_list[0])
 
 
 if __name__ == "__main__":
